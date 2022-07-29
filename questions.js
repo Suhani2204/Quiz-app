2727446a1a806d9c61bd9b57d68f037f5015caac
Suhani2204 <69099727+Suhani2204@users.noreply.github.com>
2022-07-29 20:22:03 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following JavaScript cannot do?",
+    answer: "All of the Above",
+    options: [
+        "JavaScript can react to events",
+        "JavaScript can manipulate HTML elements",
+        "JavaScript can be use to validate data",
+        " All of the Above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Whats so great about XML?",
+    answer: "Both A & B",
+    options: [
+      " Easy data exchange",
+      "High speed on network",
+      "Only B.is correct",
+      "Both A & B"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The attribute used to define a new namespace is.",
+    answer: "Xmlns",
+    options: [
+      "XMLNS",
+      "Xmlns",
+      "XmlNameSpace",
+      "XmlNs"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+];
